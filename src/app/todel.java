b6da1c555d8a4de55e0fd34131b0b25502f05d7c
yves40 <yves.toubhans@free.fr>
2021-12-25 11:44:43 +0100

    Learn how vscode java tools work

---
+++ src/app/todel.java
@@ -0,0 +1,10 @@
+package app;
+
+public class todel {
+  public static void main(String[] args) {
+    System.out.println("Hello");
+    for ( int i = 0; i < 5; i++) {
+      System.out.println("i = " + i);
+    }
+  }
+}
